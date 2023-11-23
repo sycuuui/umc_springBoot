2655102b67a4fef24493220c22f7e52c8997c8da
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-11-23 13:41:52 +0900

    feat : ch8

---
+++ src/main/java/umc/spring/converter/MemberPreferConverter.java
@@ -0,0 +1,15 @@
+package umc.spring.converter;
+
+import umc.spring.domain.mapping.MemberPrefer;
+
+public class MemberPreferConverter {
+    public static List<MemberPrefer> toMemberPreferList(List<FoodCategory> foodCategoryList){
+
+        return foodCategoryList.stream()
+                .map(foodCategory ->
+                        MemberPrefer.builder()
+                                .foodCategory(foodCategory)
+                                .build()
+                ).collect(Collectors.toList());
+    }
+}
